3b65863eb5e647d4f4373c3a26d3d938b39a060e
hex <100210094+hexress@users.noreply.github.com>
2022-07-02 23:43:17 +0300

    Update arsenal.lua

--- scripts/arsenal.lua
+++ scripts/arsenal.lua
@@ -103,278 +103,119 @@ vsls:Button(
     "Player ESP",
     function()
         DiscordLib:Notification("Enabled Feature:", "Player ESP", "Okay")
-		local VEC3 = Vector3.new
-local VEC2 = Vector2.new
-local COL3 = Color3.new
-local RGB = Color3.fromRGB
-local CFNEW = CFrame.new
-local INSTNEW = Instance.new
-local TBLINS = table.insert
-local Drawing_new = Drawing.new
-local Ray_new = Ray.new
-local TweenInfo_new = TweenInfo.new
+		assert(Drawing, "missing function/library: drawing");
 
-local Players = game:GetService("Players")
-local Workspace = game:GetService("Workspace")
-local RunService = game:GetService("RunService")
-local UserInputService = game:GetService("UserInputService")
-local TweenService = game:GetService("TweenService")
-local Camera = Workspace:FindFirstChildOfClass("Camera")
+-- services
+local run_service = game:GetService("RunService");
+local players = game:GetService("Players");
+local workspace = game:GetService("Workspace");
 
-local LocalPlayer = Players.LocalPlayer
-local Mouse = LocalPlayer:GetMouse()
+-- variables
+local camera = workspace.CurrentCamera;
+local get_pivot = workspace.GetPivot;
+local wtvp = camera.WorldToViewportPoint;
+local viewport_size = camera.ViewportSize;
+local localplayer = players.LocalPlayer;
+local cache = {};
 
-_G.PlayerLocation = function()
-	--[[
-	-- GLOBAL ESP
-	local Humanoids = {}
-	for i,v in pairs(Workspace:GetDescendants()) do
-		if v:IsA("Humanoid") or v.Name == "Humanoid" then
-			TBLINS(Humanoids, v.Parent)
-		end
-	end
-	return Humanoids;
-	]]--
-	return Players:GetChildren();
+-- locals
+local new_drawing = Drawing.new;
+local new_vector2 = Vector2.new;
+local new_color3 = Color3.new;
+local rad = math.rad;
+local tan = math.tan;
+local floor = math.floor;
+
+-- functions
+local function create_esp(player)
+    local esp = {};
+
+    esp.box = new_drawing("Square");
+    esp.box.Color = new_color3(23, 23, 23);
+    esp.box.Thickness = 1;
+    esp.box.Visible = false;
+
+    esp.tracer = new_drawing("Line");
+	esp.tracer = new_drawing("Text");
+    esp.tracer.Color = new_color3(23, 23, 23);
+    esp.tracer.Thickness = 1;
+    esp.tracer.Visible = false;
+	esp.tracer.size = 14;
+	esp.tracer.Font = Drawing.Fonts.Plex
+
+    esp.name = new_drawing("Text");
+    esp.name.Color = new_color3(23, 23, 23);
+    esp.name.Font = Drawing.Fonts.Plex;
+    esp.name.Size = 14;
+    esp.name.Center = true;
+    esp.name.Visible = false;
+
+    esp.distance = new_drawing("Text");
+    esp.distance.Color = new_color3(23, 23, 23);
+    esp.distance.Font = Drawing.Fonts.Plex;
+    esp.distance.Size = 14;
+    esp.distance.Center = true;
+    esp.distance.Visible = false;
+
+    cache[player] = esp;
 end
 
-CreateDrawing = function(ClassName)
-	return function(Props)
-		local Create = Drawing_new(ClassName)
-		for i,v in pairs(Props) do
-			Create[i] = v
-		end
-		return Create
-	end
-end;
-	
-local Drawings = {}
-function IsPartVisible(Part1, Part2)
-    local CheckPart = INSTNEW("Part")
-	CheckPart.Parent = Workspace
-	CheckPart.Name = "CheckVisWall"
-    CheckPart.Anchored = true
-    CheckPart.CanCollide = false
-    CheckPart.Transparency = 1
-    CheckPart.Size = VEC3(1.5, 1.5, 1.5) * Part2.Size
-    CheckPart.CFrame = Part2.CFrame
-    
-    local Ray = Ray_new(Part1.Position, (Part2.Position - Part1.Position).Unit * 9999)
-    local part,position = workspace:FindPartOnRay(Ray, Part1.Parent)
-	if part then
-	    if part.Name == CheckPart.Name then
-	        CheckPart:Destroy()
-	        return true
-	    end
-	end
-	CheckPart:Destroy()
-	return false
-end
-
-function GetLookVectorAndOrigin(PART)
-	local Origin = PART.CFrame
-	local LookVector = PART.CFrame.lookVector * 100
-	--local Direction = (LookVector - Origin.p).Unit * 100
-	
-	--local Ray = Ray.new(Origin.p, Direction)
-	--local _, EndPosition = workspace:FindPartOnRay(Ray)
-	
-	return {
-		Origin = Origin;
-		EndPoint = LookVector;--Workspace:Raycast(Origin, Direction.p).Position;
-	}
-end
-function Get8Corners(PART, OFF)
-	if not OFF then OFF = VEC3(1, 1, 1) end
-	local CornerVertices = {
-		{1, 1, -1},  --v1 - top front right
-		{1, -1, -1}, --v2 - bottom front right
-		{-1, -1, -1},--v3 - bottom front left
-		{-1, 1, -1}, --v4 - top front left
-		
-		{1, 1, 1},  --v5 - top back right
-		{1, -1, 1}, --v6 - bottom back right
-		{-1, -1, 1},--v7 - bottom back left
-		{-1, 1, 1}  --v8 - top back left
-	}
-	local Vertices = {}
-	local Size = PART.Size * OFF
-	for _, Vector in pairs(CornerVertices) do
-	    TBLINS(Vertices, (PART.CFrame * CFNEW(Size .X/2 * Vector[1], Size .Y/2 * Vector[2], Size .Z/2 * Vector[3])).Position)
-	end
-	return Vertices
-end
-
-function tocam(pos)
-    local PosChar, withinScreenBounds = Camera:WorldToViewportPoint(pos)
-    return {VEC2(PosChar.X, PosChar.Y), withinScreenBounds}
-end
-function GetPropPC(inst, prop)
-	local func, result = pcall(function()
-		return inst[prop]
-	end)
-	if not func then
-		return nil 
-	else 
-		return result 
-	end
-end
-function Cleanup()
-	for i,v in pairs(Drawings) do
-		v:Remove()
-		--table.remove(Drawings, i)
-	end
-	Drawings = {}
-end
-function Create3DVertex(PART, SETT)
-	local VertexPositions = Get8Corners(PART, SETT.Offset)
-	for i,v in pairs(Get8Corners(PART, VEC3(0.01, 0.01, 0.01))) do
-		if not tocam(v)[2] then return end
-	end
-	local Thickness = SETT.Thickness
-	local Transparency = SETT.Transparency
-	local Visible = SETT.Visible
-	local Color = SETT.Color
-	local Filled = SETT.Filled
-	local Positions = {
-		{
-			tocam(VertexPositions[5])[1];
-			tocam(VertexPositions[6])[1];
-			tocam(VertexPositions[2])[1];
-			tocam(VertexPositions[1])[1];
-		};
-		{
-			tocam(VertexPositions[7])[1];
-			tocam(VertexPositions[8])[1];
-			tocam(VertexPositions[4])[1];
-			tocam(VertexPositions[3])[1];
-		};
-		{
-			tocam(VertexPositions[1])[1];
-			tocam(VertexPositions[2])[1];
-			tocam(VertexPositions[3])[1];
-			tocam(VertexPositions[4])[1];
-		};
-		{
-			tocam(VertexPositions[5])[1];
-			tocam(VertexPositions[6])[1];
-			tocam(VertexPositions[7])[1];
-			tocam(VertexPositions[8])[1];
-		};
-	}
-	
-	for i = 1,#Positions do
-		local NewVertex = CreateDrawing("Quad") {
-			["Visible"] = Visible;
-			["Transparency"] = Transparency;
-			["Thickness"] = Thickness;
-			["Color"] = Color;
-			["Filled"] = Filled;
-			["PointA"] = Positions[i][1];
-			["PointB"] = Positions[i][2];
-			["PointC"] = Positions[i][3];
-			["PointD"] = Positions[i][4];
-		}
-		Drawings[#Drawings + 1] = NewVertex
-	end
-end
-
-
---[[ Initialize ESP ]]--
-spawn(function()
-	while true do
-		Cleanup()
-		
-		local func, ok = pcall(function()
-			for i,v in pairs(_G.PlayerLocation()) do
-				if v.Name ~= LocalPlayer.Name then
-					local Char = GetPropPC(v, "Character") or v or nil
-					local TeamCheck = (GetPropPC(v, "Team") ~= LocalPlayer.Team) or (GetPropPC(v, "TeamColor") ~= LocalPlayer.TeamColor) or (LocalPlayer.Team == nil)
-					if Char and TeamCheck then
-						local Root = Char:FindFirstChild("HumanoidRootPart") or nil
-						local Head = Char:FindFirstChild("Head") or nil
-						
-						if Root and Head and tocam(Head.Position)[2] then
-							--[[ Vertex ]]--
-							Create3DVertex(Root, {
-								["Offset"] = VEC3(2.25, 3, 3);
-								["Thickness"] = 1;
-								["Transparency"] = 1;
-								["Filled"] = false;
-								["Visible"] = true;
-								["Color"] = COL3(1,1,1);
-							}) 
-							
-							--[[ Health ]]--
-							Create3DVertex(Root, {
-								["Offset"] = VEC3(2.25, 3/100*Char.Humanoid.Health, 3);
-								["Thickness"] = 1;
-								["Transparency"] = 0.1;
-								["Filled"] = true;
-								["Visible"] = true;
-								["Color"] = COL3(0,1,0);
-							})
-							
-							--[[ Tracer ]]--
-							local ToCam = tocam(Head.Position)
-							if ToCam[2] then
-								local Mag = (LocalPlayer.Character.Head.Position - Head.Position).Magnitude
-								local NewLine = CreateDrawing("Line") {
-									["Visible"] = true;
-									["Transparency"] = 1;
-									["Thickness"] = 1;
-									["Color"] = RGB(255/Mag*255,255/255*Mag,0);
-									["From"] = VEC2(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y * 0.80);
-									["To"] = ToCam[1];
-								}
-								Drawings[#Drawings + 1] = NewLine
-							end
-	
-							--[[ OverHead ]]--
-							
-							--[[ IsVisible ]]--
-							local Color = COL3(1,0,0);
-							for i,v in pairs(Char:GetChildren()) do
-								if v:IsA("BasePart") then
-									if IsPartVisible(LocalPlayer.Character.Head, v) then
-										Color = COL3(0,1,0);
-									end
-								end
-							end
-							local PosPart = INSTNEW("Part")
-							PosPart.CFrame = Head.CFrame
-							PosPart.Size = VEC3(1,1,1)
-							PosPart.Transparency = 1
-							Create3DVertex(PosPart, {
-								["Offset"] = VEC3(1,1,1);
-								["Thickness"] = 1;
-								["Transparency"] = 1;
-								["Filled"] = false;
-								["Visible"] = true;
-								["Color"] = Color;
-							})
-							PosPart:Destroy()
-						end
-					end
-				end
-			end
-			return true
-		end)
-		if not func then warn(ok) end
-		
-		RunService.RenderStepped:Wait()
-	end
-end)
+local function remove_esp(player)
+    for _, drawing in next, cache[player] do
+        drawing:Remove();
     end
-)
 
-    btns:Slider(
-    "Hitchance",
-    50,
-    100,
-    75,
-    function(t)
-        print(t)
+    cache[player] = nil;
+end
+
+local function update_esp()
+    for player, esp in next, cache do
+        local character = player and player.Character;
+        if character and player.Team ~= localplayer.Team then
+            local cframe = get_pivot(character);
+            local position, visible = wtvp(camera, cframe.Position);
+
+            esp.box.Visible = visible;
+            esp.tracer.Visible = visible;
+            esp.name.Visible = visible;
+            esp.distance.Visible = visible;
+
+            if visible then
+                local scale_factor = 1 / (position.Z * tan(rad(camera.FieldOfView * 0.5)) * 2) * 100;
+                local width, height = floor(30 * scale_factor), floor(50 * scale_factor);
+                local x, y = floor(position.X), floor(position.Y);
+
+                esp.box.Size = new_vector2(width, height);
+                esp.box.Position = new_vector2(floor(x - width * 0.5), floor(y - height * 0.5));
+
+                esp.tracer.From = new_vector2(floor(viewport_size.X * 0.5), floor(viewport_size.Y));
+                esp.tracer.To = new_vector2(x, floor(y + height * 0.5));
+
+                esp.name.Text = player.Name .. "(gay)";
+                esp.name.Position = new_vector2(x, floor(y - height * 0.5 - esp.name.TextBounds.Y));
+
+                esp.distance.Text = floor(position.Z) .. " meters";
+                esp.distance.Position = new_vector2(x, floor(y + height * 0.5));
+            end
+        else
+            esp.box.Visible = false;
+            esp.tracer.Visible = false;
+            esp.name.Visible = false;
+            esp.distance.Visible = false;
+        end
+    end
+end
+
+-- initialize
+players.PlayerAdded:Connect(create_esp);
+players.PlayerRemoving:Connect(remove_esp);
+run_service.RenderStepped:Connect(update_esp);
+
+for _, player in next, players:GetPlayers() do
+    if player ~= localplayer then
+        create_esp(player);
+    end
+end
     end
 )
 
