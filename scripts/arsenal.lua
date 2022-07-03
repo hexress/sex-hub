3b2984097e865199130e5537c423bde19139a36c
hex <100210094+hexress@users.noreply.github.com>
2022-07-03 14:08:12 +0300

    Update arsenal.lua

--- scripts/arsenal.lua
+++ scripts/arsenal.lua
@@ -260,25 +260,169 @@ end)
 )
 
 misc:Button(
-    "Fly (Press X For Toggle)",
-    function()
-            DiscordLib:Notification("Enabled Feature:", "Fly", "Okay")
-		misc:Button(
     "Noclip",
     function()
             DiscordLib:Notification("Enabled Feature:", "Noclip", "Okay")
-		local runservice = game:GetService("RunService")
-
+		local User = game:GetService("UserInputService")
 local player = game:GetService("Players").LocalPlayer
-runservice.Stepped:Connect(function()
-    for i,v in pairs(player.Character:GetDescendants()) do
-        if v:IsA("BasePart") then
-            v.CanCollide = false
-        end
-    end
+local GuiService = game:GetService("StarterGui")
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+local holdingWKey = false
+local holdingSKey = false
+local holdingAKey = false
+local holdingDKey = false
+local holdingSpaceKey = false
+local holdingControlKey = false
+
+Speed_1 = -1
+
+mouse.KeyDown:connect(function(key)
+   if key == "-" then
+       Speed_1 = Speed_1 + 0.2
+       
+   end
 end)
-    end
-)
+
+mouse.KeyDown:connect(function(key)
+   if key == "=" then
+       Speed_1 = Speed_1 - 0.2
+       
+   end
+end)
+
+mouse.KeyDown:connect(function(key)
+   if key == "x" then
+       if startup  == true then
+           startup = false
+           
+       else
+           startup = true
+           local brick = Instance.new("Part", workspace)
+           brick.Size = Vector3.new(3, 2, 3)
+           brick.CFrame = player.Character.HumanoidRootPart.CFrame + Vector3.new(0, -4, 0)
+           brick.Transparency = 1
+           brick.Anchored = true
+           brick.Name = "Brick"
+           mouse.KeyDown:connect(function(key)
+               if key == "x" then
+                   brick:remove()
+               end
+           end)
+           for i = 1, math.huge do
+               brick.CFrame = player.Character.HumanoidRootPart.CFrame + Vector3.new(0, -4, 0)
+               wait(0)
+           end
+       end
+   end
+end)
+
+game:GetService('RunService').Stepped:connect(function()
+   if startup then
+       if WHeld == true then
+         player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,Speed_1)
+
+       end
+   end
+end)
+
+game:GetService('RunService').Stepped:connect(function()
+   if startup then
+   if SHeld == true then
+        player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-Speed_1)
+   end
+   end
+end)
+
+game:GetService('RunService').Stepped:connect(function()
+   if startup then
+   if AHeld == true then
+   player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(Speed_1,0,0)
+   end
+   end
+end)
+
+
+game:GetService('RunService').Stepped:connect(function()
+   if startup then
+   if DHeld == true then
+   player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(-Speed_1,0,0)
+   end
+   end
+end)
+
+
+game:GetService('RunService').Stepped:connect(function()
+   if startup then
+   if SpaceHeld == true then
+    player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(0,-Speed_1,0)
+   end
+   end
+end)
+
+game:GetService('RunService').Stepped:connect(function()
+   if startup then
+    if ControlHeld == true then
+    player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(0,Speed_1,0)
+       end
+   end
+
+end)
+
+User.InputBegan:Connect(function(inputObject)
+   if(inputObject.KeyCode==Enum.KeyCode.W) then
+       holdingWKey = true WHeld = true
+   end
+   
+   if(inputObject.KeyCode==Enum.KeyCode.S) then
+       holdingSKey = true SHeld = true
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.A) then
+       holdingAKey = true AHeld = true
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.D) then
+       holdingDKey = true DHeld = true
+   end
+   if(inputObject.KeyCode==Enum.KeyCode.LeftControl) then
+       holdingControlKey = true ControlHeld = true
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.Space) then
+       holdingSpaceKey = true SpaceHeld= true
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.W) then
+       holdingUKey = true WHeld = true
+   end
+end)
+
+User.InputEnded:Connect(function(inputObject)
+   if(inputObject.KeyCode==Enum.KeyCode.W) then
+       holdingWKey = false WHeld = false
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.S) then
+       holdingSKey = false SHeld = false    
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.A) then
+       holdingAKey = false AHeld = false    
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.D) then
+       holdingDKey = false DHeld = false    
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.LeftControl) then
+       holdingShiftKey = false ControlHeld = false    
+   end
+
+   if(inputObject.KeyCode==Enum.KeyCode.Space) then
+       holdingSpaceKey = false SpaceHeld = false
+   end
+end)
     end
 )
 
